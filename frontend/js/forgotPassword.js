be1aca327046fd9c84dc4132ee33dc40593f374a
Zoe Steinkoenig <zestein@ilstu.edu>
2025-11-07 17:18:02 -0600

    Navigation

--- frontend/js/forgotPassword.js
+++ frontend/js/forgotPassword.js
@@ -16,5 +16,5 @@ document.getElementById("verifyBtn").addEventListener("click",function(){
 
     // console.log("Verifying code:", code);
 
-    window.location.href = "../../backend/php/resetPassword";
+    window.location.href = "../../backend/php/resetPassword.php";
 });
